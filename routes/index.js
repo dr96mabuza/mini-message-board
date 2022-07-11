4a083e81204b9d1cf6e659a966c66741cc84595e
dr96mabuza <dr96mabuza@gmail.com>
2022-07-11 11:26:16 +0200

    displays messages and created form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  { text: "Hi there!", user: "Amando", added: new Date() },
+  { text: "Hello World!", user: "Charles", added: new Date() },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form", { heading: "New Message" });
+});
+
+router.post("/new", (req, res, next) => {
+  req.body;
+  messages.push({ text: Message, user: Author, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
